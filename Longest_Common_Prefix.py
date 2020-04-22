e29b995070a37c07302a65c31227535b18236df2
ahamedfofana <ahamedfofana12@gmail.com>
2020-04-22 15:54:08 -0400

    Initial commit

---
+++ Longest_Common_Prefix.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        abc_dict = {}
+        prefix = False
+        for letter in range(len(strs) - 1):
+            
+
+
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
